5aab0d8036ce2d7aacba427c010ba8df0f6ca527
Shakil Islam <shakilislamtoha@gmail.com>
2023-05-21 13:38:16 +0600

    gravira new templete file upload

---
+++ gravira_free_temp/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+     return render(request, 'index.html', {'request': request})
+
+
+def clients(request):
+     return render(request, 'contact.html', {'request': request})
+
+
+def contact(request):
+     return render(request, 'clients.html', {'request': request})
+
+
+def about(request):
+     return render(request, 'about.html', {'request': request})
+
+
